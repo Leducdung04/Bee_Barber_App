1e723b3bc6051ec0439c8fe66b956720b24e93ef
phungngocthong2004 <phungngocthong2004@gmail.com>
2024-10-23 15:38:53 +0700

    fix lich lu

--- src/Views/routers/AppNavigation.js
+++ src/Views/routers/AppNavigation.js
@@ -16,20 +16,11 @@ import colors from '../../Resources/styles/colors';
 
 import UserProfile from '../screens/UserProfile'
 import Cart from '../screens/Cart';
-import CategoriesProductsScreen from '../screens/CategoriesProductsScreen';
-import {Menu, Provider} from 'react-native-paper';
-import CircleBorder from '../components/CircleBorder';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import CategoriesSortingProduct from '../screens/CategoriesSortingProduct';
-import TopSearchScreen from '../screens/TopSearchScreen';
-import ServiceByCategoryScreen from '../screens/ServiceByCategoryScreen';
-import AppointmentScreen from '../screens/AppointmentScreen';
-import colors from '../../Resources/styles/colors';
 
 
-import UserProfile from '../screens/UserProfile'
-import Cart from '../screens/Cart';
-import AppointmentScreen from '../screens/AppointmentScreen';
+
+
+
 
 const Stack = createNativeStackNavigator();
 
